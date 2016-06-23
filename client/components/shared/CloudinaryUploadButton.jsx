107efae9e74176b885aa2fb753457e3b1fd6a7f8
DylanRichardPearson <dylanrichardpearson@gmail.com>
2016-06-22 19:30:58 -0500

    init

--- client/components/shared/CloudinaryUploadButton.jsx
+++ client/components/shared/CloudinaryUploadButton.jsx
@@ -1,29 +1,10 @@
 import React,{Component} from 'react';
-import Script from 'react-load-script';
 
 export default class CloudinaryUploadButton extends Component{
 
   render(){
     return(
-      <Script
-        url="../../scripts/cloudinary.js"
-        onCreate={this.handleScriptCreate.bind(this)}
-        onError={this.handleScriptError.bind(this)}
-        onLoad={this.handleScriptLoad.bind(this)}
-        />
+      <button onclick="cloudinaryUpload()" className="waves-effect waves-light btn">Cloudinary Upload</button>
     )
   }
-
-  handleScriptCreate() {
-    this.setState({ scriptLoaded: false })
-  }
-
-  handleScriptError() {
-    this.setState({ scriptError: true })
-  }
-
-  handleScriptLoad() {
-    this.setState({ scriptLoaded: true })
-  }
-
 }
