72bd536442a5d9760d45d16ee297a4d320421d16
DylanRichardPearson <dylanrichardpearson@gmail.com>
2016-06-22 23:33:06 -0500

    Cloudinary pushes links into the div area

--- client/layouts/ArticleEditorMain.jsx
+++ client/layouts/ArticleEditorMain.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Nav from '../components/shared/Nav.jsx';
 import ArticleNewButton from '../components/article_management/ArticleNewButton.jsx';
-import CloudinaryUploadButton from '../components/shared/CloudinaryUploadButton.jsx';
 
 export const ArticleEditorMain = ({articles}) => (
   <div>
     <Nav />
     <div className="container">
       <ArticleNewButton />
-      <CloudinaryUploadButton />
       {articles}
     </div>
   </div>
--- public/js/cloudinary_upload.js
+++ public/js/cloudinary_upload.js
@@ -4,9 +4,14 @@ $(function() {
   if(cloudinary_upload){
     cloudinary_upload.addEventListener("click", function() {
       console.log("Cloudinary upload widget opened.");
-      cloudinary.openUploadWidget({ cloud_name: 'xbdash', upload_preset: 'u2t0pjrn'},
-        function(error, result) { console.log(error, result) });
-
+      cloudinary.openUploadWidget({ cloud_name: 'xbdash', upload_preset: 'u2t0pjrn', 'folder': 'articles'},
+        function(error, result) {
+          console.log(error, result[0].url);
+          for (var i =0; i < result.length; i++){
+            console.log(result[i].url);
+            $( "#cloudinary_links" ).append(result[i].url+"<br/>");
+          }
+        });
     }, false);
   }
 });
