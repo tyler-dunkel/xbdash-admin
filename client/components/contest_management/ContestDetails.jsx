a69c4b6d82f7fc0b3fdf05118a7577ec50a6a16d
DylanRichardPearson <dylanrichardpearson@gmail.com>
2016-06-29 23:01:13 -0500

    test

--- client/components/contest_management/ContestDetails.jsx
+++ client/components/contest_management/ContestDetails.jsx
@@ -11,79 +11,7 @@ export default class ContestDetails extends TrackerReact(Component) {
         }
     }
 
-    componentDidMount() {
-        console.log("Contest details component mounted");
-        $('select').material_select();
 
-
-        $(document).ready(function(){
-            $( ".add-row" ).click(function(){
-                var $clone = $( "#prizeArea" ).first().clone();
-                $clone.append( "<button type='button' class='remove-row'>-</button>" );
-                $clone.insertBefore( ".add-row" );
-            });
-
-            $( ".remove-row" ).on("click", ".remove-row", function(){
-                console.log("remove clicked");
-                $(this).parent().remove();
-            });
-        });
-    }
-
-    componentWillUnmount() {
-        this.state.subscription.xbdContests.stop();
-    }
-
-    getContest() {
-        return xbdContests.findOne(this.props.id);
-    }
-
-    render() {
-        let contest = this.getContest();
-        if (this.props.id === 'new') {
-            return (
-                <form>
-                    <div>
-                        <div>
-                            <label for="Title">Title</label>
-                            <input type="text" id="Title" className="validate"/>
-                        </div>
-                        <div>
-                            <label for="ContestToken">Contest Token</label>
-                            <input type="text" id="ContestToken" className="validate"/>
-                        </div>
-                        <div>
-                            <label for="StartDate">Start Date</label>
-                            <input type="datetime-local" id="StartDate"/>
-                        </div>
-                        <div>
-                            <label for="StartDate">End Date</label>
-                            <input type="datetime-local" id="EndDate"/>
-                        </div>
-                        <div>
-                            <label for="SendPrizeDate">Send Prize Date</label>
-                            <input type="datetime-local" id="SendPrizeDate"/>
-                        </div>
-                    </div>
-                    <div>
-                        <select id="Status">
-                            <option value="Active" selected>Active</option>
-                            <option value="Disabled">Disabled</option>
-                        </select>
-                        <label for="Status">Status</label>
-                    </div>
-                    <h5>Prizes</h5>
-                    <div id="prizeArea">
-                        <input type="text" id="PrizeTitle" placeholder="Prize Title"/>
-                        <select id="IsPremium">
-                            <option value="true" selected>true</option>
-                            <option value="false">false</option>
-                        </select>
-                        <input type="text" id="PrizeImageUrl" placeholder="Prize Image Url"/>
-                    </div>
-                    <button type="button" className="add-row">+</button>
-                    <button type="submit" className="btn waves-effect waves-light">Submit</button>
-                </form>
 
             )
         }
