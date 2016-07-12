8f4f425e03b9b882904e91fd2685e12759523420
Dylan Pearson <dpearson@conversantmedia.com>
2016-07-12 16:05:55 -0500

    Blaze interacting with React Components correctly

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -1,14 +1,26 @@
 import React,{Component} from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class Home extends Component {
+export default class Home extends TrackerReact(Component) {
   render(){
-    console.log(Meteor.user());
+
+    if(Meteor.user() === undefined){
+      console.log("user is undefined");
+      return(
+        <div>Loading</div>
+      )
+    }
+
+    console.log(Meteor.userId());
+    console.log(Meteor.user().username);
+    let username = Meteor.user().username;
     return (
       <div>
         <div className="section no-pad-bot" id="index-banner">
           <div className="container">
-            <h1 className="header center green-text">Hello, username</h1>
+            <h1 className="header center green-text">Hello, {username}</h1>
             <div className="row center">
             
             </div>
