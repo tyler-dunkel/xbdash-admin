a538e441ddb3d50f53a10d89d978bfd1921ebdf3
DylanRichardPearson <dylanrichardpearson@gmail.com>
2016-08-04 22:59:04 -0500

    Featured cards in progress

--- client/components/featured_management/Featured.jsx
+++ client/components/featured_management/Featured.jsx
@@ -1,49 +1,11 @@
 import React, {Component} from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
-import SearchBar from 'react-search-bar';
 import FeaturedArea from './FeaturedArea.jsx';
 
 export default class Featured extends TrackerReact(Component) {
-    onChange(input, resolve) {
-        const matches = {
-            'macbook a': [
-                'macbook air 13 case',
-                'macbook air 11 case',
-                'macbook air charger'
-            ],
-            'macbook p': [
-                'macbook pro 13 case',
-                'macbook pro 15 case',
-                'macbook pro charger'
-            ]
-        };
-        setTimeout(() => {
-            const suggestions = matches[Object.keys(matches).find((partial) => {
-                return input.match(new RegExp(partial), 'i');
-            })] || ['macbook', 'macbook air', 'macbook pro'];
-
-            resolve(suggestions.filter((suggestion) =>
-                suggestion.match(new RegExp('^' + input.replace(/\W\s/g, ''), 'i'))
-            ));
-        }, 25);
-    }
-
-    onSearch(input) {
-        if (!input) return;
-        console.info(`Searching "${input}"`);
-    }
-
-    onSubmit(submitted) {
-        console.log(submitted);
-    }
     render() {
         return (
             <div>
-                <SearchBar
-                    placeholder="Search for the new featured content"
-                    onChange={this.onChange}
-                    onSearch={this.onSearch}
-                    onSubmit={this.onSubmit} />
                 <FeaturedArea />
             </div>
         );
--- client/components/featured_management/FeaturedCard.jsx
+++ client/components/featured_management/FeaturedCard.jsx
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
+import Search from '../shared/Search.jsx';
 
 export default class FeaturedCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            subscription: {
+                xbdNews: Meteor.subscribe("allxbdnews"),
+                xbdContests: Meteor.subscribe("allxbdcontests"),
+                xbdAnnouncements: Meteor.subscribe("allxbdannouncements")
+            }
+        }
+    }
+
     render() {
         console.log(this.props.type);
         return (
-            <div className="card sticky-action">
-
-                <div className="card-action"></div>
-
-                <div className="card-reveal"></div>
+            <div className="card medium">
+                <div className="card-image waves-effect waves-block waves-light">
+                    <img className="activator" src="images/office.jpg" />
+                </div>
+                <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">{this.props.type}<i className="material-icons right">more_vert</i></span>
+                    <p><a href="#">This is a link</a></p>
+                </div>
+                <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">Update {this.props.type}<i className="material-icons right">close</i></span>
+                    <Search type={this.props.type}/>
+                </div>
             </div>
+
         )
     }
 }
