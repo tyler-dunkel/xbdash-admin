a5bc91ac4a8ba01dac1b17a6a3ebf03f6434d0d7
Dylan Pearson <dpearson@conversantmedia.com>
2016-08-10 16:22:57 -0500

    Check if loaded

--- client/components/shared/ArticleSearch.jsx
+++ client/components/shared/ArticleSearch.jsx
@@ -1,43 +1,41 @@
 import React, {Component} from 'react';
 import {SearchSource} from 'meteor/meteorhacks:search-source';
 
-let options = {keepHistory: 1000 * 60 * 5, localSearch: true};
+let options = { keepHistory: 1000 * 60 * 5, localSearch: true };
 let fields = ['articleTitle', 'author'];
 SearchArticles = new SearchSource('articles', this.fields, this.options);
 
 export default class ArticleSearch extends Component {
-    constructor() {
-        super();
-
-        console.log(options);
-        console.log(fields);
-        console.log(SearchArticles);
-    }
-
     getArticles() {
-        let articles = SearchArticles.getData({
+        return SearchArticles.getData({
             transform(matchText, regExp) {
                 return matchText.replace(regExp, "<b>$&</b>")
             },
             sort: { isoScore: -1 }
         });
-        console.log(articles);
-        return articles;
     }
 
     isLoading() {
-        return SearchArticles.getStatus().loading;
+        return SearchArticles.getStatus();
     }
 
     handleChange(e) {
-        console.log(fields);
-        console.log(options);
         var text = $(e.target).val().trim();
-        console.log(text);
         SearchArticles.search(text);
     }
 
     render() {
+        console.log(this.isLoading().loaded);
+        if (this.isLoading().loaded!=true) {
+            return (
+                <div>
+                    <input type="text" id="search-box" placeholder="search articles here" onKeyUp={this.handleChange} />
+                    <div>
+                        Searching...
+                    </div>
+                </div>
+            )
+        }
         return (
             <div>
                 <input type="text" id="search-box" placeholder="search articles here" onKeyUp={this.handleChange} />
---
+++ client/components/shared/ArticleSearchResult.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class ArticleSearchResult extends Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
